bcc0d5726d33bcf761d4d86d71fb0fc1a503da22
Lucas André <lucascoder14@gmail.com>
2025-06-01 12:16:52 -0300

    Prática 1 - Parte 4 - Navegação com mais uma tela e campos extras

---
+++ app/src/main/java/com/example/weatherapp/RegisterActivity.kt
@@ -0,0 +1,121 @@
+package com.example.weatherapp
+
+import android.app.Activity
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class RegisterActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    RegisterPage(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterPage(modifier: Modifier = Modifier) {
+    var nomeUsuario by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var senha by rememberSaveable { mutableStateOf("") }
+    var senhaConfirmada by rememberSaveable { mutableStateOf("") }
+
+    val activity = LocalContext.current as? Activity
+    Column(
+        modifier = modifier.padding(16.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(
+            text = "Bem-vindo/a!",
+            fontSize = 24.sp
+        )
+        Spacer(
+            modifier = modifier.size(24.dp)
+        )
+        OutlinedTextField(
+            value = nomeUsuario,
+            label = { Text(text = "Digite nome de usuário") },
+            modifier = modifier.fillMaxWidth(0.9f),
+            onValueChange = { nomeUsuario = it },
+        )
+        OutlinedTextField(
+            value = email,
+            label = { Text(text = "Digite seu e-mail") },
+            modifier = modifier.fillMaxWidth(0.9f),
+            onValueChange = { email = it },
+        )
+        OutlinedTextField(
+            value = senha,
+            label = { Text(text = "Digite sua senha") },
+            modifier = modifier.fillMaxWidth(0.9f),
+            onValueChange = { senha = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        OutlinedTextField(
+            value = senhaConfirmada,
+            label = { Text(text = "Confirme sua senha") },
+            modifier = modifier.fillMaxWidth(0.9f),
+            onValueChange = { senhaConfirmada = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Row(modifier = modifier) {
+            Button(
+                onClick = {
+                    Toast.makeText(activity, "Usuário Cadastrado com Sucesso!!", Toast.LENGTH_LONG).show()
+                    activity?.startActivity(
+                        Intent(activity, LoginActivity::class.java).setFlags(
+                            FLAG_ACTIVITY_SINGLE_TOP
+                        )
+                    )
+                    activity?.finish()
+                },
+                enabled = email.isNotEmpty() && senha.isNotEmpty() && senhaConfirmada.isNotEmpty() && nomeUsuario.isNotEmpty() && senha == senhaConfirmada
+            ) {
+                Text("Registrar")
+            }
+            Button(
+                onClick = { email = ""; senha = ""; senhaConfirmada = ""; nomeUsuario = "" }
+            ) {
+                Text("Limpar")
+            }
+        }
+    }
+}
