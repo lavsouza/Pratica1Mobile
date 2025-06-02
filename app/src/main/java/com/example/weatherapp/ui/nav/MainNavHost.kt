efdfbb4487b3eb1dd62a3c8f0ebcd17ce24b8c41
Lucas André <lucascoder14@gmail.com>
2025-06-02 16:15:03 -0300

    Prática 2 - Parte 2 - navegação entre telas

---
+++ app/src/main/java/com/example/weatherapp/ui/nav/MainNavHost.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapp.ui.nav
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.weatherapp.ui.HomePage
+import com.example.weatherapp.ui.ListPage
+import com.example.weatherapp.ui.MapPage
+
+@Composable
+fun MainNavHost(navController: NavHostController) {
+    NavHost(navController, startDestination = Route.Home) {
+        composable<Route.Home> { HomePage() }
+        composable<Route.List> { ListPage() }
+        composable<Route.Map> { MapPage() }
+    }
+}
